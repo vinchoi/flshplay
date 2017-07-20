7ac44699828a7f717179e85f6f21d539e6dcb483
caihaohxian <caihaohxian@bluemoon.com.cn>
2017-07-20 17:33:02 +0800

    modify modeles

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from datetime import datetime, date
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Product(db.Model):
@@ -32,3 +33,17 @@ class Product_sub(db.Model):
 
     def __repr__(self):
         return "<Product_sub %r>" % self.package
+
+class User(db.Model):
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+        
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
